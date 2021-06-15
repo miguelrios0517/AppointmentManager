2c48ff7676fa63f089a221efac82896bb1941f11
Miguel Rios <miguel.rios1997@gmail.com>
2021-06-15 15:53:31 -0700

    pushed from windows

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,12 +1,12 @@
-import React, { useContext, useState, useEffect } from 'react'
-import { auth } from '../firebase'
-
 //Auth context is an intermediate between your firebase program (returns firebase app.auth -
 // i.e., access to your backend) and your app.js, the parent component of your frontend. By using
 // React.creatContext and passing it the value = {currentUser, signup}, we grant every child nested 
 // within the <AuthProvider /> component a reference to the currentUser state and the signup() method
 //
- 
+
+import React, { useContext, useState, useEffect } from 'react'
+import { auth } from '../firebase'
+
 const AuthContext = React.createContext()
 
 //this is a helper function that allows to use the context (i.e., gives us access to the context )
@@ -18,7 +18,6 @@ export function AuthProvider({ children }) {
     const [currentUser, setCurrentUser] = useState()
     const [loading, setLoading] = useState(true)
 
-    //helper function that calls the creatUserWith... method in the auth api
     function signup(email, password) {
         auth.createUserWithEmailAndPassword(email, password) //this returns a promise wich we can use inside of Signup.js
     }
@@ -44,3 +43,4 @@ export function AuthProvider({ children }) {
         </AuthContext.Provider>
     )
 }
+ 
