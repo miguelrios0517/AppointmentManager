13fa136b8760df4819ee9d047f99ad28d210fdb8
Miguel Rios <miguel.rios1997@gmail.com>
2022-04-17 14:16:56 -0700

    adding sass

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -2,6 +2,7 @@ import React, { Component, useState } from 'react';
 import ApptPreview from './appt-preview';
 import ApptCarousel from './appt-carousel';
 import ToDo from './to-do'; 
+import Widget from "./widget/Widget";
 import { LocalDiningOutlined, PinDropSharp } from '@material-ui/icons';
 import { Card, Alert, Button } from 'react-bootstrap'
 import { useAuth } from '../contexts/AuthContext'
@@ -14,6 +15,12 @@ export default function Dashboard(props) {
                 Dashboard Overview
             </header>
             <div className = "main">
+                <div className="widgets">
+                    <Widget type="user" />
+                    <Widget type="order" />
+                    <Widget type="earning" />
+                    <Widget type="balance" />
+                </div>
                 <ApptPreview appointments={props.appointments} />  
                 <ToDo />
                 <ApptCarousel />
